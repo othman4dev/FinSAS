8708d2a3785d3869af30e543f8eccf0d6d58996d
othman <109024890+hotmanX813@users.noreply.github.com>
2023-09-20 18:57:41 +0100

    Add files via upload

--- ToDoList.c
+++ ToDoList.c
@@ -3,29 +3,30 @@
 #include<string.h>
 #include<math.h>
 
-struct deadline
+typedef struct
 {
         int jour;
         int mois;
         int year;
-};
+}deadline;
 
-struct data
+typedef struct
 {
     char name[35];
     char date[35];
     int id;
-    char discription[50];
-    struct deadline deadline[100];
+    char discription[100];
     char status[35];
-};
-struct data dt[100];
+}data;
+data dt[100];
+deadline dl[100];
+
+int count = 0;
 
 int main() {
     menu();
 }
 int menu() {
-    int n = 1;
     printf("\n==========================\n\tTo do list :\n==========================\n");
     printf("\n - choisissez une operation :\n\n");
     printf("[1]- Ajouter des taches.\n");
@@ -40,7 +41,7 @@ int menu() {
     switch (a)
     {
     case 1:
-        add(n);
+        add();
     case 2:
         show();
     case 3:
@@ -56,28 +57,27 @@ int menu() {
         exit(EXIT_SUCCESS);
     default:
         printf("\nInvalide choice\n");
+        main();
     }
 }
 int add() {
-    for (int i = 0; i < 100; i++)
+    for (int i = count; i < 100; i++)
     {
-        dt[i].deadline[i].jour = 0;
-        dt[i].deadline[i].mois = 0;
-        dt[i].deadline[i].year = 0;
-        struct data dt[i];
         printf("Tache numero %d : \n",i+1);
         printf("Nom de tache :\n> ");
         scanf(" %29[^\n]",&dt[i].name);
-        dt[i].id = i;
+        printf("Description de tache :\n> ");
+        scanf(" %29[^\n]",&dt[i].discription);
+        dt[i].id = i + 1;
         printf("Identifiant de tache : %d\n",dt[i].id);
         printf("\nEntrer le deadline de tache :\n\n-Entrer la date sous format 'jj/mm/aaaa'-\n");
         for (int j = 0; j < 10; j++)
         {
-            scanf("%d/%d/%d",&dt[i].deadline[i].jour,&dt[i].deadline[i].mois,&dt[i].deadline[i].year);
-            if(dt[i].deadline[i].year < 2023 || dt[i].deadline[i].jour < 1 || dt[i].deadline[i].jour > 31 || dt[i].deadline[i].mois > 12 || dt[i].deadline[i].mois < 1 ) {
+            scanf("%d/%d/%d",&dl[i].jour,&dl[i].mois,&dl[i].year);
+            if(dl[i].year < 2023 || dl[i].jour < 1 || dl[i].jour > 31 || dl[i].mois > 12 || dl[i].mois < 1 ) {
                 printf("Invalide date !!\n Veillez rentrer une date valide. \n");
             }
-            else if (dt[i].deadline[i].mois == 2 && dt[i].deadline[i].jour > 28)
+            else if (dl[i].mois == 2 && dl[i].jour > 28)
             {
                 printf("Invalide date !!\n Veillez rentrer une date valide. \n");
             }
@@ -86,34 +86,32 @@ int add() {
                 j = 10;
             }
         }
+        
         printf("status de tache :\n\n");
+        int s = 0;
         printf("\t[1]- A realiser\n");
         printf("\t[2]- En cours de realisation\n");
         printf("\t[3]- Finalisee\n> ");
-        int x;
-        scanf("%d",&x);
-        char stat1[20] = "A realiser";
-        char stat2[30] = "En cours de realisation";
-        char stat3[30] = "Finalisee";
-        if (x == 1)
+        scanf("%d",&s);
+        if (s == 1)
         {
-
             strcpy(dt[i].status,"A realiser");
         }
-        else if (x == 2) 
+        else if (s == 2) 
         {
             strcpy(dt[i].status,"En cours de realisation");
         }
-        else if (x == 3) 
+        else if (s == 3) 
         {
             strcpy(dt[i].status,"Finalisee");
         } else 
         {
-            printf("La tache a modifiee comme 'A realiser'\n");
+            printf("La tache a configurait comme 'A realiser'\n");
             strcpy(dt[i].status,"A realiser");
         }
-        printf("Tache %d a ete ajoutee \t:\nNom du tache :\t%s\nStatus de tache :\t%s\nDeadline de tache :\t%d / %d / %d\nIdentifiant de tache :\t%d\n",i,dt[i].name,dt[i].status,dt[i].deadline[i].jour,dt[i].deadline[i].mois,dt[i].deadline[i].year,dt[i].id);
-        printf("Ajouter une autre tache :\n\t[1]-yes\n\t[2]-non\n");
+        printf("Tache %d a ete ajoutee \t:\nNom du tache :\t%s\nStatus de tache :\t%s\nDeadline de tache :\t%d / %d / %d\nIdentifiant de tache :\t%d\n",i+1,dt[i].name,dt[i].status,dl[i].jour,dl[i].mois,dl[i].year,dt[i].id);
+        count++;
+        printf("\nAjouter une autre tache ?:\n\t[1]-oui\n\t[2]-non\n> ");
         int y;
         scanf("%d",&y);
         if(y == 2) {
@@ -126,7 +124,8 @@ int add() {
             main();
         }
         }
-    }
+
+}
 int search(){
 
 }
@@ -134,7 +133,37 @@ int change() {
 
 }
 int show() {
-
+    for (int i = 0; i <= count; i++)
+    {
+        printf("\nListe des taches :\n\n");
+        printf("%d- %s > %s.\n\t( %d / %d / %d )\n",i+1,dt[i].name,dt[i].status,dl[i].jour,dl[i].mois,dl[i].year);
+    }
+    printf("\n\n[1]- Triee Alphabitiquemet.\n[2]- Triee par deadline.\n[3]- affichier dont le deadline est dans 3 jours ou moins.");
+    int a ;
+    scanf("%d",&a);
+    if (a == 1)
+    {
+        int index = 0;
+        for (int i = 0; i < count; i++)
+        {
+            if (dt[i].name[0]>dt[i+1].name[0])
+            {
+                index = i;
+            } else {
+                index = i+1;
+            }
+            printf("index is : %d",index);
+             
+        }
+        
+    } else if (a == 2)
+    {
+        
+    } else if (a == 3) {
+    } else {
+        printf("\nInvalide choix.\n");
+    }
+    main();
 }
 int delete() {
 
